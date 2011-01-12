06a0b24214293e775e7f1b61d0193607f5cf4d56
zach gage <zgage@optonline.net>
2011-01-11 23:25:20 -0500

    commented tutorial
    and slightly improved it (sorting now so charecters are layered properly)

--- example/testApp.cpp
+++ example/testApp.cpp
@@ -1,49 +1,58 @@
 #include "testApp.h"
 
+// comparison routine for sort...
+bool sortVertically(  basicSprite * a, basicSprite * b ) { 
+	return a->pos.y > b->pos.y; 
+}   
+
 //--------------------------------------------------------------
 void testApp::setup(){
-	ofSetFrameRate(30);
+	ofSetFrameRate(30); //lets run at 30 fps!
+
+	spriteRenderer = new ofxSpriteSheetRenderer(1, 10000, 0, 32); //declare a new renderer with 1 layer, 10000 tiles per layer, default layer of 0, tile size of 32
+	spriteRenderer->loadTexture("spriteSheetExample.png", 256, GL_NEAREST); // load the spriteSheetExample.png texture of size 256x256 into the sprite sheet. set it's scale mode to nearest since it's pixel art
 	
-	//1 layer, 10000 tiles per layer, default layer of 0, tile size of 32
-	spriteRenderer = new ofxSpriteSheetRenderer(1, 10000, 0, 32); 
-	spriteRenderer->loadTexture("spriteSheetExample.png", 256, GL_NEAREST);
-	
-	ofEnableAlphaBlending();
+	ofEnableAlphaBlending(); // turn on alpha blending. important!
 }
 
 //--------------------------------------------------------------
 void testApp::update(){
-	spriteRenderer->clear();
-	spriteRenderer->update(ofGetElapsedTimeMillis());
+	spriteRenderer->clear(); // clear the sheet
+	spriteRenderer->update(ofGetElapsedTimeMillis()); //update the time in the renderer, this is necessary for animations to advance
 	
-	if(sprites.size()>0)
+	sort( sprites.begin(), sprites.end(), sortVertically ); // sorts the sprites vertically so the ones that are lower are drawn later and there for in front of the ones that are higher up
+	
+	if(sprites.size()>0) // if we have sprites
 	{
-		for(int i=sprites.size()-1;i>=0;i--)
+		for(int i=sprites.size()-1;i>=0;i--) //go through them
 		{
-			sprites[i].pos.y+=sprites[i].speed;
+			sprites[i]->pos.y+=sprites[i]->speed; //add their speed to their y
 			
-			if(sprites[i].pos.y > ofGetHeight()+32)
-				sprites.erase(sprites.begin()+i);
-			else
-				spriteRenderer->addCenteredTile(&sprites[i].animation, sprites[i].pos.x, sprites[i].pos.y);
+			if(sprites[i]->pos.y > ofGetHeight()+16) //if they are past the bottom of the screen
+			{
+				delete sprites[i]; //delete them
+				sprites.erase(sprites.begin()+i); // remove them from the vector
+			}
+			else //otherwise
+				spriteRenderer->addCenteredTile(&sprites[i]->animation, sprites[i]->pos.x, sprites[i]->pos.y); // add them to the sprite renderer (add their animation at their position, there are a lot more options for what could happen here, scale, tint, rotation, etc, but the animation, x and y are the only variables that are required)
 		}
 	}
 	
 	for(int i=0;i<10;i++) //lets add ten sprites every frame and fill the screen with an army
 	{
-		basicSprite newSprite;
-		newSprite.pos.set(ofRandom(0,ofGetWidth()),0);
-		newSprite.speed=ofRandom(1,5);
-		newSprite.animation = walkAnimation;
-		newSprite.animation.frame_duration /= newSprite.speed;
-		sprites.push_back(newSprite);
+		basicSprite * newSprite = new basicSprite(); // create a new sprite
+		newSprite->pos.set(ofRandom(0,ofGetWidth()),-16); //set its position
+		newSprite->speed=ofRandom(1,5); //set its speed
+		newSprite->animation = walkAnimation; //set its animation to the walk animation we declared
+		newSprite->animation.frame_duration /= newSprite->speed; //adjust its frame duration based on how fast it is walking (faster = smaller)
+		sprites.push_back(newSprite); //add our sprite to the vector
 	}
 }
 
 //--------------------------------------------------------------
 void testApp::draw(){
 	
-	spriteRenderer->draw();
+	spriteRenderer->draw(); //draw the sprites!
 }
 
 //--------------------------------------------------------------
--- example/testApp.h
+++ example/testApp.h
@@ -6,24 +6,25 @@
 
 #include "ofxSpriteSheetRenderer.h"
 
+//create a new animation. This could be done optinally in your code andnot as a static, just by saying animation_t walkAnimation; walkAnimation.index =0, walkAnimation.frame=0 etc. I find this method the easiest though
 static animation_t walkAnimation = 
-{	0,	/* index		*/
-	0,	/* frame		*/
-	4,	/* totalframes	*/			
-	1,	/* width		*/	
-	1,	/* height		*/
-	75,	/* frameduration*/	
-	0,	/* nexttick		*/
-	-1,	/* loops		*/
-	-1,	/* finalindex	*/
-	1	/* frameskip	*/
+{	0,	/* .index			(int) - this is the index of the first animation frame. indicies start at 0 and go left to right, top to bottom by tileWidth on the spriteSheet		*/
+	0,	/* .frame			(int) - this is the current frame. It's an internal variable and should always be set to 0 at init													*/
+	4,	/* .totalframes		(int) - the animation length in frames																												*/			
+	1,	/* .width			(int) - the width of each animation frame in tiles																									*/	
+	1,	/* .height			(int) - the height of each animation frame in tiles																									*/	
+	75,	/* .frameduration	(unsigned int) - how many milliseconds each frame should be on screen. Less = faster																*/	
+	0,	/* .nexttick		(unsigned int) - the next time the frame should change. based on frame duration. This is an internal variable and should always be set to 0 at init	*/
+	-1,	/* .loops			(int) - the number of loops to run. -1 equals infinite. This can be adjusted at runtime to pause animations, etc.									*/
+	-1,	/* .finalindex		(int) - the index of the final tile to draw when the animation finishes. -1 is the default so total_frames-1 will be the last frame.				*/
+	1	/* .frameskip		(int) - the incrementation of each frame. 1 should be set by default. If you wanted the animation to play backwards you could set this to -1, etc.	*/
 };
 
 //a quick and dirty sprite implementation
 struct basicSprite {
-	animation_t animation;
-	ofPoint pos;
-	float speed;
+	animation_t animation;	// a variable to store the animation this sprite uses
+	ofPoint pos;			// the position on the screen this sprite will be drawn at
+	float speed;			// the speed at which this sprite moves down the screen
 };
 
 class testApp : public ofBaseApp{
@@ -42,8 +43,8 @@ class testApp : public ofBaseApp{
 		void windowResized(int w, int h);
 
 	
-	ofxSpriteSheetRenderer * spriteRenderer;
-	vector<basicSprite> sprites;
+	ofxSpriteSheetRenderer * spriteRenderer;	// our spriteRenderer
+	vector<basicSprite *> sprites;				// our vector of sprites
 };
 
 #endif
